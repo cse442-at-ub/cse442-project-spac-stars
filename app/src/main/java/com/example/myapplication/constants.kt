0a3d89dd9e536a7ee36bf555ae6301d139154495
shijiazheng123 <szheng39@buffalo.edu>
2021-03-13 14:58:55 -0500

    created GUI for filtered list

--- app/src/main/java/com/example/myapplication/constants.kt
+++ app/src/main/java/com/example/myapplication/constants.kt
@@ -9,6 +9,22 @@ object constants {
         "Pre+IPO" to "A3",
         "Warrants+(Testing)" to "A2"
     )
+    val categoryInfoColumn: Map<String, String> = mapOf(
+        "Pre+LOI" to "P",
+        "Definitive+Agreement" to "O",
+        "Option+Chads" to "P",
+        "Pre+Unit+Split" to "G",
+        "Pre+IPO" to "C",
+        "Warrants+(Testing)" to "M"
+    )
+    val categoryInfoLabel: Map<String, String> = mapOf(
+        "Pre+LOI" to "Current Volume",
+        "Definitive+Agreement" to "Current Volume",
+        "Option+Chads" to "Current Volume",
+        "Pre+Unit+Split" to "Est. Trust Value",
+        "Pre+IPO" to "Est. Trust Size",
+        "Warrants+(Testing)" to "Current Volume"
+    )
     val sheetID: String = "1dZOPswJcmPQ5OqTw7LNeTZOXklnmD-n7fyohbkRSsFE"
     val apikey: String = "AIzaSyCZP2fBW638Gip01kDHMbHLaM84hWwU7uo"
 }
