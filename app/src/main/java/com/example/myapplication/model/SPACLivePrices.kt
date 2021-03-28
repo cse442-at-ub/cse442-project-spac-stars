5985cfb94a95f5fe973bcd9dca7b5fbacbb59c71
agkon <annkonna@buffalo.edu>
2021-03-27 20:33:53 -0400

    User Story #5 - Live Stock Prices of all SPACs

---
+++ app/src/main/java/com/example/myapplication/model/SPACLivePrices.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.model
+
+data class SPACLivePrices(val stringResourceId1: String,
+                          val stringResourceId2: String,
+                          val stringResourceId3: String
+)
