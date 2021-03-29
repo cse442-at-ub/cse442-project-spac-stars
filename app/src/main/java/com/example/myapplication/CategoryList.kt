6a8bacf8441c80e6face0b58566263368e1f8666
shijiazheng123 <47539262+shijiazheng123@users.noreply.github.com>
2021-03-29 17:34:19 -0400

    Revert "Saved list"

--- app/src/main/java/com/example/myapplication/CategoryList.kt
+++ app/src/main/java/com/example/myapplication/CategoryList.kt
@@ -8,15 +8,17 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.constants.apikey
+
+import com.example.myapplication.constants.worksheetsStartingRow
 import com.example.myapplication.constants.categoryInfoColumn
 import com.example.myapplication.constants.categoryInfoLabel
+import com.example.myapplication.constants.apikey
 import com.example.myapplication.constants.sheetID
 import com.example.myapplication.constants.sortingOrder
-import com.example.myapplication.constants.worksheetsStartingRow
-import org.json.JSONObject
+
 import java.net.URL
-import kotlin.concurrent.thread
+import org.json.*
+import kotlin.concurrent.*
 
 
 
@@ -36,7 +38,9 @@ class CategoryList : AppCompatActivity() {
             println(results.joinToString())
             this@CategoryList.runOnUiThread(Runnable {
                 val listAdapter = TickerListAdapter(results, categoryInfoLabel[SPACtype])
+                println(listAdapter)
                 val viewList: RecyclerView = findViewById(R.id.recyclerView)
+                println("view set")
                 viewList.adapter = listAdapter
                 viewList.layoutManager = LinearLayoutManager(this)
                 viewList.setHasFixedSize(true)
@@ -118,5 +122,8 @@ class CategoryList : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+
+
 
 }
--- app/src/main/java/com/example/myapplication/model/SPACLivePrices.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.model
-
-data class SPACLivePrices(val stringResourceId1: String,
-                          val stringResourceId2: String,
-                          val stringResourceId3: String
-)
